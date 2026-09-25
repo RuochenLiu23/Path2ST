8f1f385817fc7e61b6255cf92f1a3518768ebf13
RuochenLiu <144750736+RuochenLiu23@users.noreply.github.com>
2026-09-25 09:48:02 +0100

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,32 @@
+import os
+
+# Encoder feature dimensions per backbone
+ENCODER_FEATURE_DIMS = {
+    'uni': 1536,
+    'conch': 512,
+    'resnet18': 512,
+}
+
+DEFAULT_DATA_ROOT = os.environ.get('Path2ST_DATA_ROOT', './data')
+
+# Dataset configuration including recommended validation/test slides
+DATASETS = {
+    'PRAD': {
+        'dir_name': 'PRAD',
+        'val_slides': 'MEND145',
+        'test_slides': 'MEND145',
+        'recommended_encoder': 'uni'
+    },
+    'her2st': {
+        'dir_name': 'her2st',
+        'val_slides': 'SPA148',
+        'test_slides': 'SPA148',
+        'recommended_encoder': 'uni'
+    },
+    'mouse_brain': {
+        'dir_name': 'mouse_brain',
+        'val_slides': 'NCBI667',
+        'test_slides': 'NCBI667',
+        'recommended_encoder': 'uni'
+    }
+}
